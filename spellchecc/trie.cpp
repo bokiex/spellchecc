cf5098d001b46442223d0ead39f400c0b28e364f
bOk <kohbokyan@gmail.com>
2019-01-11 20:38:29 +0800

    Implemented search function

--- spellchecc/trie.cpp
+++ spellchecc/trie.cpp
@@ -1,9 +1,11 @@
 #include "trie.h"
+#include <string>
 
 const int ALPHABET_SIZE = 26;
 
 // node attributes
-struct TrieNode {
+struct TrieNode 
+{
 	struct TrieNode *children[ALPHABET_SIZE];
 
 	// true if node represents the end of a word
@@ -11,8 +13,8 @@ struct TrieNode {
 };
 
 // returns new trie node and initialises 26 null children
-struct TrieNode *getNode(void) {
-	
+struct TrieNode *getNode(void) 
+{
 	struct TrieNode *pNode = new TrieNode;
 
 	pNode->isEndOfWord = false;
@@ -23,4 +25,38 @@ struct TrieNode *getNode(void) {
 	return pNode;
 }
 
-// 
+// loops through key and checks each 
+// value to see if it exists
+// if it doesnt, create a new node 
+// else, node.isEndOfWord = true
+void insert(struct TrieNode *root, std::string key) 
+{
+	struct TrieNode *node = root;
+
+	for (auto i : key) {
+		auto index = i - 'a';
+		if (!node->children[index])
+			node->children[index] = getNode();
+
+		node = node->children[index];
+	}
+
+	// mark last node as leaf
+	node->isEndOfWord = true;
+}
+
+bool search(struct TrieNode *root, std::string key) 
+{
+	struct TrieNode *node = root;
+
+	for (auto i : key) 
+	{
+		auto index = i - 'a';
+		if (!node->children[index]) {
+			return false;
+		}
+		node = node->children[index];
+	}
+
+	return (node != NULL && node->isEndOfWord);
+}
